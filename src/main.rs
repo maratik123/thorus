abecdf4e73d3a5f14784805df65a83a5be53ed03
Marat Buharov <marat.buharov@gmail.com>
2024-02-11 19:58:35 +0300

    create device

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use tracing::{debug, enabled, Level};
-use vulkano::device::QueueFlags;
+use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
 use vulkano::instance::{Instance, InstanceCreateInfo};
 use vulkano::VulkanLibrary;
 
@@ -22,18 +22,36 @@ fn main() {
 
     if enabled!(Level::DEBUG) {
         for (pos, family) in physical_device.queue_family_properties().iter().enumerate() {
-            debug!("found a queue family at index {pos}: {family:?}");
+            debug!("found a queue family with property at index {pos}: {family:?}");
         }
     }
 
     let queue_family_index = physical_device
         .queue_family_properties()
         .iter()
-        .position(|queue_family_property| {
-            queue_family_property
+        .position(|queue_family_properties| {
+            queue_family_properties
                 .queue_flags
                 .contains(QueueFlags::GRAPHICS)
         })
-        .expect("couldn't find a graphical queue family");
+        .expect("couldn't find a graphical queue family") as u32;
     debug!("selected queue family index: {queue_family_index}");
+
+    let (device, mut queues) = Device::new(
+        physical_device,
+        DeviceCreateInfo {
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
+                ..QueueCreateInfo::default()
+            }],
+            ..DeviceCreateInfo::default()
+        },
+    )
+    .expect("failed to create device");
+    debug!("created device: {device:?}");
+
+    let queue = queues
+        .next()
+        .expect("at least one queue expected for device");
+    debug!("device queue: {queue:?}");
 }
