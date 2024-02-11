fba267b2850d1836c38911aed85797bb1ca3b2ec
Marat Buharov <marat.buharov@gmail.com>
2024-02-11 15:39:50 +0300

    create instance

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,8 @@
-fn main() {}
+use vulkano::instance::{Instance, InstanceCreateInfo};
+use vulkano::VulkanLibrary;
+
+fn main() {
+    let library = VulkanLibrary::new().expect("no local Vulkan library");
+    let instance =
+        Instance::new(library, InstanceCreateInfo::default()).expect("failed to create instance");
+}
