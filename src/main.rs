f7c48197972983d69989045796c40c368ae0e0f0
Marat Buharov <marat.buharov@gmail.com>
2024-02-11 16:35:52 +0300

    tracing; queue props

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
+use tracing::debug;
 use vulkano::instance::{Instance, InstanceCreateInfo};
 use vulkano::VulkanLibrary;
 
 fn main() {
+    tracing_subscriber::fmt::init();
+
     let library = VulkanLibrary::new().expect("no local Vulkan library");
+    debug!("initialized library: {library:?}");
 
     let instance =
         Instance::new(library, InstanceCreateInfo::default()).expect("failed to create instance");
+    debug!("initialized instance: {instance:?}");
 
-    let physical_devices = instance
+    let physical_device = instance
         .enumerate_physical_devices()
         .expect("could not enumerate physical devices")
         .next()
         .expect("no devices available");
+    debug!("chosen physical device: {physical_device:?}");
+
+    for family in physical_device.queue_family_properties() {
+        debug!(
+            "found a queue family with {:?} queue(s)",
+            family.queue_count
+        );
+    }
 }
