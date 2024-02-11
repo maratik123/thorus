340a6be3e4ea4f0fa2208c265d8b47029948ab6e
Marat Buharov <marat.buharov@gmail.com>
2024-02-11 15:43:52 +0300

    enumerate phys devs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@ use vulkano::VulkanLibrary;
 
 fn main() {
     let library = VulkanLibrary::new().expect("no local Vulkan library");
+
     let instance =
         Instance::new(library, InstanceCreateInfo::default()).expect("failed to create instance");
+
+    let physical_devices = instance
+        .enumerate_physical_devices()
+        .expect("could not enumerate physical devices")
+        .next()
+        .expect("no devices available");
 }
